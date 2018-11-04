59406a13f7b4041b16d1fef604e4a12840b9cee3
lffloyd <lfmatosmelo@id.uff.br>
2018-11-04 18:56:39 -0200

    Processamento de arq. de grafo iniciado; arq. lido.

--- main.rkt
+++ main.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require graph)
 
 ; Verifica se um elemento x eh um programa PDL.
 (define (programa? x)
@@ -32,25 +33,48 @@
   (display "Programas: ")
   (writeln programas))
 
-(define (criar-valoracoes in vals)
+(define (pegar-elementos l)
+  (cond [(not (equal? l "")) (map (lambda (x) (string-trim x ","))
+                                  (string-split (string-replace (string-trim l "}") " = {" " ") " "))]
+        [else ""]))
+
+(define (limpar-string s)
+  (cond [(equal? s "") ""]
+        [else (map (lambda (x) (string-split (string-trim (string-trim x ",") ")") ", "))
+                   (string-split (string-replace (string-trim s "}") " = {" " ") " ("))]))
+
+(define (simbolizar-elems l)
+  (cond [(empty? l) l]
+        [else (cons (string->symbol (first l)) (simbolizar-elems (rest l)))]))
+
+(define (pegar-tuplas l)
+  (define l2 (limpar-string l))
+  (cond [(equal? l "") ""]
+        [else (rest (map (lambda (x) (cons (string->symbol (first (first l2))) (simbolizar-elems x))) l2))]))
+
+(define (criar-relacoes in rels)
   (define l (read-line in))
-  (cond [(eof-object? l) cons l vals]
-        [else (cons l (criar-valoracoes in vals))]))
+  (cond [(eof-object? l) cons l rels]
+        [else (cons (pegar-tuplas l) (criar-relacoes in rels))]))
+
+(define (pegar-mundos m)
+  (cond [(equal? m "") false] [else (regexp-match* #rx"[a-z0-9]+" (string-trim (string-trim m "}") "W = {"))]))
+
+(define (criar-mundos l)
+  (cond [(equal? l "") ""]
+        [else (map (lambda (x) (string->symbol x)) (pegar-mundos l))]))
 
 (define (criar-grafo nome-arq)
   (display "Arquivo de grafo: ")
   (writeln nome-arq)
   (define in (open-input-file nome-arq))
-  (define mun-str (read-line in))
-  (define rel-str (read-line in))
-  (define vals-str (criar-valoracoes in'()))
+  (define mun-str (criar-mundos (read-line in)))
+  (define relalp-str (criar-relacoes in '()))
   (close-input-port in)
   (display "Mundos: ")
   (writeln mun-str)
   (display "Relacoes em alpha: ")
-  (writeln rel-str)
-  (display "Valoracoes: ")
-  (writeln vals-str))
+  (writeln relalp-str))
 
 ; Pede ao usuario os nomes dos arquivos e chama as funcoes correspondentes para o processamento dos mesmos.
 (define (main mensagem)
